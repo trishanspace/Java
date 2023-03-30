c26f6f6ca4dc587a0d599f1e8b9c8f878918a0e5
Nataliia <nataliyatrishechkina@mail.ru>
2023-03-30 11:11:29 +0300

    added new version

--- task2.java
+++ task2.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;;
 
 // 2. Дан массив nums = [3,2,2,3] и число val = 3.
 // Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
@@ -7,47 +7,19 @@ import java.util.Arrays;
 
 public class task2 {
     public static void main(String[] args) {
-        getNewArray();
-        getNewArray2();
+        moveElementToEnd(3, 2, 2, 3, 3);
     }
 
-    private static void getNewArray() {
-        int[] arr = { 3, 2, 2, 3 };
-        int number = 3;
-        int temp = 0;
-
-        for (int i = 0; i <= arr.length; i++) {
-            for (int j = -1; j >= arr.length; j--) {
-                if (arr[i] == number && arr[j] != number) {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                } else if (arr[i] == number && arr[j] == number) {
-                    j--;
-                } else {
-                    return;
-                }
+    private static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+        for (int i = 0, lastElementIndex = array.size() - 1; i <= lastElementIndex;) {
+            if (array.get(i) == toMove) {
+                array.remove(i);
+                array.add(toMove);
+                lastElementIndex--;
+            } else {
+                i++;
             }
         }
-        System.out.println(Arrays.toString(arr));
-    }
-
-    private static void getNewArray2() {
-        int[] arr = { 3, 2, 2, 3 };
-        int number = 3;
-        int temp = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = -1; j > arr.length; j--) {
-                while (arr[j] == number) {
-                    j--;
-                }
-                if (arr[i] == number && arr[j] != number) {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
-        System.out.println(Arrays.toString(arr));
+        return array;
     }
 }
