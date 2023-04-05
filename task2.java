e85983b4fbd13e51655e29c588943eee22df078f
Nataliia <nataliyatrishechkina@mail.ru>
2023-04-06 01:16:56 +0300

    added LL in t2

--- task2.java
+++ task2.java
@@ -1,3 +1,21 @@
+import java.util.LinkedList;
+import java.util.List;
+// import java.util.Stack;
+
+
+//  2. Пусть дан LinkedList с несколькими элементами. Реализуйте метод, 
+//  который вернет “перевернутый” список.
+/**
+ * task2
+ */
 public class task2 {
-    
+
+    public static void main(Integer[] args) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+    }
 }
