520b011c87663d3a5d1d1474cc23c1ddd25e0273
Nataliia <nataliyatrishechkina@mail.ru>
2023-04-06 18:29:34 +0300

    added draft version t1

--- task1.java
+++ task1.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+// 1. Реализовать консольное приложение, которое:
+
+// Принимает от пользователя и “запоминает” строки.
+// Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, 
+// а первая - последней.
+// Если введено revert, удаляет предыдущую введенную строку из памяти.
+
 public class task1 {
-    
+
+    public static void main(String list) {
+        // LinkedList<String> list = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        String[] inputs = scanner.nextLine().split(" ");
+        StringBuilder sb = new StringBuilder(list);
+
+        if (inputs.equals("print")) {
+
+            char first = sb.charAt(0);
+            sb.setCharAt(0, sb.charAt(sb.length() - 1));
+            sb.setCharAt(sb.length() - 1, first);
+            sb.toString();
+            System.out.println(sb);
+
+        } else if (inputs.equals("revert")) {
+            sb.deleteCharAt(sb.length() - 1);
+            System.out.println(sb);
+        } else{
+            System.out.println(inputs);
+        }
+    }
 }
