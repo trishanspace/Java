6def0c09bbfb7f9116ffbe1d846073c8dfb8b872
Nataliia <nataliyatrishechkina@mail.ru>
2023-04-13 12:01:59 +0300

    added final v t2

--- task2.java
+++ task2.java
@@ -1,40 +1,61 @@
+import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.Map;
 // 2. Пусть дан список сотрудников:
 // Написать программу, которая найдёт и выведет повторяющиеся имена с количеством повторений. 
 // Отсортировать по убыванию популярности.
 public class task2 {
-
-    private HashMap<String, Integer> names = new HashMap<>();
-
-    public void namesStaff() {
-        putName("Иван Иванов");
-        putName("Светлана Петрова");
-        putName("Кристина Белова");
-        putName("Анна Мусина");
-        putName("Анна Крутова");
-        putName("Иван Юрин");
-        putName("Петр Лыков");
-        putName("Павел Чернов");
-        putName("Петр Чернышов");
-        putName("Мария Федорова");
-        putName("Марина Светлова");
-        putName("Мария Савина");
-        putName("Мария Рыкова");
-        putName("Марина Лугова");
-        putName("Анна Владимирова");
-        putName("Иван Мечников");
-        putName("Петр Петин");
-        putName("Иван Ежов");
-
-    }
-    
-    public void putName(String name) {
-        if (names.containsKey(name)) {
-            names.put(name, names.get(name) + 1);
-        } else{
-        names.put(name, 1);
+    public static ArrayList<String> getName(String[] list) {
+        ArrayList<String> listName = new ArrayList<>();
+        for (String el : list) {
+            String[] elList = el.split(" ");
+            listName.add(elList[0]);
         }
-        System.out.println(names);
+        return listName;
+    }
+
+    public static Map<String, Integer> getMap(ArrayList<String> name) {
+        Map<String, Integer> mapName = new HashMap<>();
+        for (int i = 0; i < name.size(); i++) {
+            int count = 1;
+            for (int j = i + 1; j < name.size(); j++) {
+                if (name.get(i).equals(name.get(j))) count += 1;
+            }
+            if (mapName.containsKey(name.get(i)) == false) mapName.put(name.get(i), count);
+        }
+        return mapName;
+    }
+
+    public static void nameRepeat(Map<String, Integer> map) {
+        for (var item : map.entrySet()) {
+            if (item.getValue() > 1) System.out.printf("%s: %d \n", item.getKey(), item.getValue());
+        }
+    }
+
+    public static void sortName(Map<String, Integer> map) {
+        Map<Integer, ArrayList<String>> sortMap = new HashMap<>();
+        ArrayList<Integer> listCount = new ArrayList<>();
+        for (var item : map.entrySet()) {
+            if (listCount.contains(item.getValue()) == false) listCount.add(item.getValue());
+        }
+        listCount.sort(null);
+        for (int i = listCount.size() - 1; i > -1; i--) {
+            for (var item : map.entrySet()) {
+                if (listCount.get(i) == item.getValue())
+                    System.out.printf("%s : %d \n", item.getKey(), item.getValue());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] emploees = new String[]{"Иван Иванов", "Светлана Петрова", "Кристина Белова", "Анна Мусина", "Анна Крутова", 
+        "Иван Юрин", "Петр Лыков", "Павел Чернов", "Петр Чернышов", "Мария Федорова", "Марина Светлова", 
+        "Мария Савина", "Мария Рыкова", "Марина Лугова", "Анна Владимирова", "Иван Мечников", "Петр Петин"};
+        ArrayList<String> emploeesName = getName(emploees);
+        Map<String, Integer> mapName = getMap(emploeesName);
+        System.out.println("Повторения: ");
+        nameRepeat(mapName);
+        System.out.println("По убыванию популярности: ");
+        sortName(mapName);
     }
 }
